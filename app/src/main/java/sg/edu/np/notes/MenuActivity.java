ac2731298a541f7439bd072a9f6689dec0932ea3
somebodylol123 <chowyuncong@gmail.com>
2021-06-07 00:22:53 +0800

    ALertDialog

--- app/src/main/java/sg/edu/np/notes/MenuActivity.java
+++ app/src/main/java/sg/edu/np/notes/MenuActivity.java
@@ -1,7 +1,9 @@
 package sg.edu.np.notes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -19,6 +21,7 @@ public class MenuActivity extends AppCompatActivity
     Button recyclerBtn;
     Button cdBtn;
     Button signOutBtn;
+    Button alertBtn;
 
 
     @Override
@@ -31,6 +34,8 @@ public class MenuActivity extends AppCompatActivity
         recyclerBtn = findViewById(R.id.recyclerBtn);
         cdBtn = findViewById(R.id.cdtBtn);
         signOutBtn = findViewById(R.id.signOutBtn);
+        alertBtn = findViewById(R.id.alertBtn);
+
         //random number generator
         int ranNum = ranInt(10000);
         toastBtn.setOnClickListener(new View.OnClickListener() {
@@ -61,6 +66,13 @@ public class MenuActivity extends AppCompatActivity
             }
         });
 
+        alertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alert();
+            }
+        });
+
         cdBtn.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -88,5 +100,25 @@ public class MenuActivity extends AppCompatActivity
         Random random = new Random();
         int ranNum = random.nextInt(limit);
         return ranNum;
+    }
+
+    private void alert()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert Dialog");
+        builder.setMessage("Press yes to go signout");
+        builder.setCancelable(false);
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MenuActivity.this, "Sign out successfully!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", null);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
